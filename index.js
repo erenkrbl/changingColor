3837db66f08079eb8f1667b8f16813cd69ad8f36
Joseph Abraham <ekurubal24@gmail.com>
2021-05-11 19:29:59 +0300

    Changed background color

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ buttonElement.addEventListener("click", changeColors)
 
 function changeColors(){
     const randomIndex = Math.floor(Math.random() * colors.length);
+    document.querySelector("body").style.backgroundColor = colors[randomIndex];
     console.log(randomIndex);
 }
 
